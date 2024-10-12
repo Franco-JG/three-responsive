8b1e5eb678081c4f296af1bc8ab95c7865e40100
Franco-JG <jeftefranco@outlook.com>
2024-10-12 11:15:00 -0600

    chore: Se ha modificado la resolucion del canvas utilizando sus atributos width y height

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { scene } from './shapes/scene.ts'
 import './style.css'
 
 const canvas = <HTMLCanvasElement> document.querySelector('#c')
+const canvas2 = <HTMLCanvasElement> document.querySelector('#c2')
 
 scene(canvas)
+scene(canvas2)
--- src/shapes/scene.ts
+++ src/shapes/scene.ts
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 export const scene = (canvas: HTMLCanvasElement) => {
 
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+	renderer.setPixelRatio(window.devicePixelRatio)
 
 	const fov = 75;
 	const aspect = 2; // the canvas default
@@ -10,7 +11,7 @@ export const scene = (canvas: HTMLCanvasElement) => {
 	const far = 5;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 	camera.position.z = 2;
-
+	
 	const scene = new THREE.Scene();
 
   const cube = new THREE.Mesh(
@@ -19,7 +20,24 @@ export const scene = (canvas: HTMLCanvasElement) => {
   )
   scene.add(cube)
 
+	function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if (needResize) {
+			renderer.setSize(width, height, false);
+		}
+		return needResize;
+	}
+
 	function animate() {
+		
+		if(resizeRendererToDisplaySize(renderer)){
+			const canvas =  renderer.domElement;
+			camera.aspect = canvas.clientWidth/canvas.clientHeight
+			camera.updateProjectionMatrix()
+		}
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
